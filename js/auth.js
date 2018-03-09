8391b44eb67ccc6c897b02e6084102f6e7bd88dc
Dave Benvenuti <davebenvenuti@gmail.com>
2018-03-09 13:34:16 -0800

    Use timingSafeEqual to guard against timing attacks

--- js/auth.js
+++ js/auth.js
@@ -1,5 +1,11 @@
+import { timingSafeEqual } from 'crypto';
+
+const apiKey = Buffer.from(process.env.API_KEY);
+
 export default function auth(request, response, next) {
-  if(request.get("Authorization") == process.env.API_KEY) {
+  const authHeader = Buffer.from(request.get("Authorization"));
+
+  if(timingSafeEqual(authHeader, apiKey)) {
     next();
   } else {
     response.status(401).json({
